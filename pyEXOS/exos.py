52468ee66d61339530b97b3ea3d20ddead752e38
Elisa Jasinska <elisa@jasinska.de>
2018-01-23 00:20:39 -0400

    more logging, bugfix where added and deleted commands need to be in order

--- pyEXOS/exos.py
+++ pyEXOS/exos.py
@@ -63,8 +63,10 @@ class EXOS(object):
                                          ssh_config_file=self.ssh_config_file)
             self.device.timeout = self.timeout
         except (NetMikoTimeoutException, NetMikoAuthenticationException):
-            self.log.error(exc_info=True)
+            self.log.error('error', exc_info=True)
             raise
+
+        self.log.info("connected")
 
     def close(self):
         """
@@ -74,6 +76,7 @@ class EXOS(object):
         """
         if hasattr(self.device, 'remote_conn'):
             self.device.remote_conn.close()
+        self.log.info("disconnected")
 
     def is_alive(self):
         """
@@ -83,6 +86,7 @@ class EXOS(object):
         """
         if self.device:
             is_alive = self.device.remote_conn.transport.is_active()
+            self.log.info("connection is still active")
         else:
             is_alive = False
         return {'is_alive': is_alive}
@@ -100,11 +104,11 @@ class EXOS(object):
         :raises: EXOSException
         """
 
-        # scp to device in the future candidate.xsf
-
         if filename is None and config is None:
-            raise EXOSException("at least one of the following attributes has to be \
-                provided: 'filename' or 'config'")
+            msg = "at least one of the following attributes has to be \
+                  provided: 'filename' or 'config'"
+            self.log.error(msg)
+            raise EXOSException(msg)
 
         self.candidate_config = ''
 
@@ -115,6 +119,7 @@ class EXOS(object):
             self.candidate_config = config.splitlines()
 
         self.candidate_config = [line.strip() for line in self.candidate_config]
+        self.log.info("candidate_config loaded")
 
     def commit_config(self):
         """
@@ -124,12 +129,10 @@ class EXOS(object):
         :raises: EXOSException, ValueError, NetMikoTimeoutException
         """
 
-        # load file in the future
-        # save configuration as-script backup
-        # load script configuration candidate
-
         if self.candidate_config is None:
-            raise EXOSException("Candidate config not loaded")
+            msg = "Candidate config not loaded"
+            self.log.error(msg)
+            raise EXOSException(msg)
 
         try:
             # make a backup of the running_config
@@ -140,12 +143,14 @@ class EXOS(object):
             for command in commands:
                 output = self.device.send_command_timing(command)
                 if 'Invalid input detected' in output:
-                    raise EXOSException("Error while sending the '{0}' command:"
-                                        "\n{1}".format(command, output))
+                    msg = "Error while sending the '{0}' command: \n{1}".format(command, output)
+                    self.log.error(msg)
+                    raise EXOSException(msg)
             self.candidate_config = None
         except (EXOSException, ValueError, NetMikoTimeoutException):
-            self.log.error(exc_info=True)
+            self.log.error('error', exc_info=True)
             raise
+        self.log.info("commit merge done")
 
     def discard_config(self):
         """
@@ -153,10 +158,9 @@ class EXOS(object):
 
         :return: None
         """
-        # remove file on device in the future
-        # rm candidate.xsf
 
         self.candidate_config = None
+        self.log.info("candidate_config discarded")
 
     def get_running_config(self):
         """
@@ -170,8 +174,9 @@ class EXOS(object):
             self.running_config = output.splitlines()
             self.running_config = [line.strip() for line in self.running_config]
         except (ValueError, IndexError, IOError):
-            self.log.error(exc_info=True)
+            self.log.error('error', exc_info=True)
             raise
+        self.log.info("running_config aquired")
 
     def compare_merge_config(self):
         """
@@ -185,7 +190,11 @@ class EXOS(object):
         :raises: EXOSException
         """
         if self.candidate_config is None:
-            raise EXOSException("Candidate config not loaded")
+            msg = "Candidate config not loaded"
+            self.log.error(msg)
+            raise EXOSException(msg)
+
+        self.log.info("comparing config for commit merge")
 
         # make sure we have the running_config
         self.get_running_config()
@@ -200,6 +209,7 @@ class EXOS(object):
                                     tofile='candidate_config.conf',
                                     n=0,
                                     lineterm='')
+        self.log.info("diff aquired")
         return '\n'.join(list(diff))
 
     def compare_replace_config(self):
@@ -210,7 +220,11 @@ class EXOS(object):
         :raises: EXOSException
         """
         if self.candidate_config is None:
-            raise EXOSException("Candidate config not loaded")
+            msg = "Candidate config not loaded"
+            self.log.error(msg)
+            raise EXOSException(msg)
+
+        self.log.info("comparing config for commit replace")
 
         # make sure we have the running_config
         self.get_running_config()
@@ -220,7 +234,7 @@ class EXOS(object):
                                             fromfile='running_config.conf',
                                             tofile='candidate_config.conf',
                                             lineterm='')
-
+        self.log.info("diff aquired")
         replace_diff = '\n'.join(replace_diff)
         return replace_diff
 
@@ -233,20 +247,21 @@ class EXOS(object):
         :raises: EXOSException, ValueError, NetMikoTimeoutException
         """
         if self.candidate_config is None:
-            raise EXOSException("Candidate config not loaded")
+            msg = "Candidate config not loaded"
+            self.log.error(msg)
+            raise EXOSException(msg)
 
         try:
             # fetch running_config to generate delete commands for it
             self.get_running_config()
-
             # generate delta commands
             self.candidate_config = self._generate_commands()
-
             # commit new candidate_config
             self.commit_config()
         except (EXOSException, ValueError, NetMikoTimeoutException):
-            self.log.error(exc_info=True)
+            self.log.error('error', exc_info=True)
             raise
+        self.log.info("commit replace done")
 
     def rollback(self):
         """
@@ -256,7 +271,9 @@ class EXOS(object):
         :raises: EXOSException, ValueError, NetMikoTimeoutException
         """
         if self.running_config is None:
-            raise EXOSException("No previous running_config to roll back to present")
+            msg = "No previous running_config to roll back to present"
+            self.log.error(msg)
+            raise EXOSException(msg)
 
         try:
             # load the previous running_config to get back to the previous state
@@ -269,7 +286,7 @@ class EXOS(object):
             # commit_replace the config
             self.commit_replace_config()
         except (EXOSException, ValueError, NetMikoTimeoutException):
-            self.log.error(exc_info=True)
+            self.log.error('error', exc_info=True)
             raise
 
     def _generate_commands(self):  # noqa
@@ -279,11 +296,16 @@ class EXOS(object):
         :return: (list) delete statements
         """
         replace_diff = self.compare_replace_config()
+        commands_add = []
+        command_del = None
+        commands_del = []
         commands = []
         acl_commands = []
         unconfigure_acl_commands = []
-        command = None
+
         for line in replace_diff.splitlines():
+
+            # generate commands to be added
             if line.startswith('+') and ('configure access-list' in line and 'ports' in line):
                 direction = line.split()[-1]
                 if direction not in ['ingress', 'egress']:
@@ -293,23 +315,30 @@ class EXOS(object):
                     delimiter = ','
                 else:
                     delimiter = ':'
+                # one line generates multiple delete statements here
                 for port in ports.split(delimiter):
                     acl_command = '{0} {1} {2}'.format(' '.join(line.split()[:4]), port, direction)
                     acl_commands.append(acl_command[1::])
                     if acl_command[1::] not in self.running_config:
-                        commands.append(acl_command[1::])
+                        commands_add.append(acl_command[1::])
             elif line.startswith('+'):
-                command = line[1::]
-            elif line.startswith('-') and 'running_config.conf' not in line:
+                commands_add.append(line[1::])
+
+            # generate commands to be removed
+            if line.startswith('-') and 'running_config.conf' not in line:
+
                 if ('create vlan' in line or
                     'create eaps' in line or
                     'create meter' in line):
-                    command = line[1::].replace('create', 'delete')
+                    command_del = line[1::].replace('create', 'delete')
+
                 elif ('fdbentry' in line or
                       'fdb' in line):
-                    command = line[1::].replace('create', 'delete').split('port')[0].strip()
+                    command_del = line[1::].replace('create', 'delete').split('port')[0].strip()
+
                 elif 'configure iproute' in line:
-                    command = line[1::].replace('add', 'delete')
+                    command_del = line[1::].replace('add', 'delete')
+
                 elif ('disable iproute ipv4 compression' in line or
                       'disable iproute ipv6 compression' in line or
                       'disable igmp snooping vlan' in line or
@@ -321,7 +350,8 @@ class EXOS(object):
                       'disable lldp ports' in line or
                       'disable learning port' in line or
                       'disable snmp access vr' in line):
-                    command = line[1::].replace('disable', 'enable')
+                    command_del = line[1::].replace('disable', 'enable')
+
                 elif ('enable log target' in line or
                       'enable sntp-client' in line or
                       'enable tacacs' in line or
@@ -329,90 +359,133 @@ class EXOS(object):
                       'enable iproute ipv4 compression' in line or
                       'enable iproute ipv6 compression' in line or
                       'enable stpd' in line):
-                    command = line[1::].replace('enable', 'disable')
+                    command_del = line[1::].replace('enable', 'disable')
+
                 elif 'enable eaps' == line[1::]:
-                    command = line[1::].replace('enable', 'disable') + "\ny"
+                    command_del = line[1::].replace('enable', 'disable') + "\ny"
+
                 elif 'enable sflow' in line:
                     if len(line.split()) > 3:
                         line_parts = line.split()
-                        command = ' '.join(line_parts[:-1])[1::].replace('enable', 'disable')
+                        command_del = ' '.join(line_parts[:-1])[1::].replace('enable', 'disable')
                     else:
-                        command = line[1::].replace('enable', 'disable')
+                        command_del = line[1::].replace('enable', 'disable')
+
                 elif 'configure tacacs primary' in line:
-                    command = 'unconfigure tacacs server primary'
+                    command_del = 'unconfigure tacacs server primary'
+
                 elif 'configure tacacs secondary' in line:
-                    command = 'unconfigure tacacs server secondary'
-                elif 'configure access-list' in line and 'ports' in line:
+                    command_del = 'unconfigure tacacs server secondary'
+
+                elif ('configure access-list' in line and
+                      'ports' in line):
                     unconfigure_acl_commands.append(line[1::])
+
                 elif 'configure access-list zone' in line:
                     acl_zone_parts = line.split('application')
                     application = acl_zone_parts[1].split()[0]
-                    command = '{0} {1} {2}'.format(''.join(acl_zone_parts[0])[1::],
-                                                   'delete application', application)
+                    command_del = '{0} {1} {2}'.format(''.join(acl_zone_parts[0])[1::],
+                                                       'delete application', application)
+
                 elif 'enable sharing' in line:
                     line_part = line.split('grouping')
-                    command = line_part[0][1::].replace('enable', 'disable')
+                    command_del = line_part[0][1::].replace('enable', 'disable')
+
                 elif ('configure eaps' in line and
                       'port' in line or
                       'configure mstp region' in line):
-                    command = ' '.join(line.split()[:-1])[1::].replace('configure', 'unconfigure')
+                    command_del = ' '.join(line.split()[:-1])[1::].replace('configure', 'unconfigure')
+
                 elif 'configure pim register-checksum-to include-data' in line:
-                    command = 'configure pim register-checksum-to exclude-data'
+                    command_del = 'configure pim register-checksum-to exclude-data'
+
                 elif ('configure eaps shared-port' in line or
                       'configure sflow collector' in line or
                       'configure pim' in line):
-                    command = line[1::].replace('configure', 'unconfigure')
+                    command_del = line[1::].replace('configure', 'unconfigure')
+
                 elif ('configure syslog' in line or
                       'configure dns-client' in line):
-                    command = line[1::].replace('add', 'delete')
+                    command_del = line[1::].replace('add', 'delete')
+
                 elif 'configure snmpv3 add community' in line:
-                    command = ' '.join(line.split()[:5])[1::].replace('add', 'delete')
+                    command_del = ' '.join(line.split()[:5])[1::].replace('add', 'delete')
+
                 elif 'configure sflow agent' in line:
-                    command = line[1::].split('ipaddress')[0].replace('configure', 'unconfigure')
+                    command_del = line[1::].split('ipaddress')[0].replace('configure', 'unconfigure')
+
                 elif 'configure sflow ports' in line:
-                    command = line[1::].split('sample-rate')[0].replace('configure', 'unconfigure')
+                    command_del = line[1::].split('sample-rate')[0].replace('configure', 'unconfigure')
+
                 elif 'configure sflow' in line:
-                    command = "unconfigure sflow"
+                    command_del = "unconfigure sflow"
+
                 elif 'create ldap domain' in line:
                     if 'default' in line:
-                        command = ' '.join(line.split()[:-1])[1::].replace('create', 'delete')
+                        command_del = ' '.join(line.split()[:-1])[1::].replace('create', 'delete')
                     else:
-                        command = line[1::].replace('create', 'delete')
+                        command_del = line[1::].replace('create', 'delete')
+
                 elif 'configure ldap domain' in line:
-                    command = ' '.join(line.split()[:-2])[1::].replace('configure', 'delete')
+                    command_del = ' '.join(line.split()[:-2])[1::].replace('configure', 'delete')
+
                 elif 'configure sntp-client primary' in line:
-                    command = 'unconfigure sntp-client primary'
+                    command_del = 'unconfigure sntp-client primary'
+
                 elif 'configure sntp-client secondary' in line:
-                    command = 'unconfigure sntp-client secondary'
+                    command_del = 'unconfigure sntp-client secondary'
+
                 elif ('configure snmpv3 add target-addr' in line or
                       'configure snmpv3 add target-params' in line):
-                    command = ' '.join(line.split()[:5])[1::]
-                    command = re.sub('\sadd', ' delete', command)
+                    command_del = ' '.join(line.split()[:5])[1::]
+                    command_del = re.sub('\sadd', ' delete', command_del)
+
                 elif 'configure vlan' in line and 'untagged' in line:
-                    command = ' '.join(line.split()[:-1])[1::].replace('add', 'delete')
+                    command_del = ' '.join(line.split()[:-1])[1::].replace('add', 'delete')
+
                 elif 'configure vlan' in line and 'tagged' in line:
-                    command = ' '.join(line.split()[:-1])[1::].replace('add', 'delete')
+                    command_del = ' '.join(line.split()[:-1])[1::].replace('add', 'delete')
+
                 elif 'description-string' in line:
-                    command = ' '.join(line.split()[:-1])[1::].replace('configure', 'unconfigure')
+                    command_del = ' '.join(line.split()[:-1])[1::].replace('configure', 'unconfigure')
+
                 elif 'configure vlan' in line and 'ipaddress' in line:
-                    command = ' '.join(line.split()[:-2])[1::].replace('configure', 'unconfigure')
+                    command_del = ' '.join(line.split()[:-2])[1::].replace('configure', 'unconfigure')
+
                 elif 'configure snmp sysName' in line:
-                    command = "configure snmp sysName \"''\""
+                    command_del = "configure snmp sysName \"''\""
+
                 elif 'configure snmp sysLocation' in line:
-                    command = "configure snmp sysLocation \"''\""
+                    command_del = "configure snmp sysLocation \"''\""
+
                 elif 'configure snmp sysContact' in line:
-                    command = 'configure snmp sysContact "support@extremenetworks.com, +1 888 257 3000"'
+                    command_del = 'configure snmp sysContact "support@extremenetworks.com, +1 888 257 3000"'
+
                 elif 'configure timezone' in line:
-                    command = 'configure timezone 0'
-            if command:
-                if command not in commands:
-                    commands.append(command)
+                    command_del = 'configure timezone 0'
+
+                if not command_del:
+                    msg = "I don't know how to remove the following command: '{0}'".format(line)
+                    self.log.warn(msg)
+
+                # add resulting command to list
+                if command_del:
+                    if command_del not in commands_del:
+                        commands_del.append(command_del)
+
+        # sanitize unconfigure_acl_commands in list
         for line in list(set(unconfigure_acl_commands)):
             if line not in list(set(acl_commands)):
                 direction = line.split()[-1]
                 acl_command = '{0} {1}'.format(' '.join(line.split()[:3]), direction)
-                commands.append(acl_command.replace('configure', 'unconfigure'))
+                commands_del.append(acl_command.replace('configure', 'unconfigure'))
 
+        # assemble commands to be added and deleted
+        # delete first to avoid removing a newly added line
+        commands.extend(commands_del)
+        commands.extend(commands_add)
         commands = [x for x in commands if not x.startswith('#') and not x.startswith('+')]
         commands = [x for x in commands if not x == '']
+
+        self.log.info("replace commands generated")
         return commands
